e1cf585e33d204ff60f169ae3b8bb2e20229a159
Yakult <destin.tax@outlook.jp>
2018-08-26 10:41:48 -0700

    削除機能の追加

--- js/common.js
+++ js/common.js
@@ -7,12 +7,6 @@ const vm = new Vue({
   el: '#todo',
   data: {
     items: [
-        { title: '領収書を準備する', isChecked: true },
-        { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
-        { title: '参加者の人数を確認する', isChecked: false },
-        { title: 'ピザを注文する', isChecked: false },
-        { title: '参加費のお釣りを準備する', isChecked: false },
-        { title: '会場設営をする', isChecked: false },
     ],
     newItemTitle: '',
     canTnter: false
@@ -33,6 +27,14 @@ const vm = new Vue({
         this.newItemTitle = '';
         this.canTnter = false;
         this.saveTodo();
+    },
+    deleteTodo: function(ele){
+        if(confirm(ele.title + 'を削除しますか？')){
+            this.items = this.items.filter(function(item){
+                return item.title != ele.title;
+            })
+            this.saveTodo();
+        }
     },
     saveTodo: function(){
         localStorage.setItem('items', JSON.stringify(this.items));
