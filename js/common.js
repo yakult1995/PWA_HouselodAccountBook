2f9ae6538e4156f879c968d324b6856bfc953b1f
Yakult <destin.tax@outlook.jp>
2018-08-26 10:25:02 -0700

    Vue.jsによるTodo

--- js/common.js
+++ js/common.js
@@ -2,3 +2,40 @@ function checkVersion(){
     var current_version = $('#current_version').text().replace('.', '-').split('-')[1];
     alert(current_version);
 }
+
+const vm = new Vue({
+  el: '#todo',
+  data: {
+    items: [
+        { title: '領収書を準備する', isChecked: true },
+        { title: 'Vue.jsハンズオンの資料を作る', isChecked: true },
+        { title: '参加者の人数を確認する', isChecked: false },
+        { title: 'ピザを注文する', isChecked: false },
+        { title: '参加費のお釣りを準備する', isChecked: false },
+        { title: '会場設営をする', isChecked: false },
+    ],
+    newItemTitle: ''
+  },
+  mounted: function(){
+    this.loadTodo();
+  },
+  methods: {
+    addTodo: function(newTitle){
+      this.items.push({
+        title: newTitle,
+        isChecked: false
+      });
+      this.newItemTitle = '';
+      this.saveTodo();
+    },
+    saveTodo: function(){
+      localStorage.setItem('items', JSON.stringify(this.items));
+    },
+    loadTodo: function(){
+      this.items = JSON.parse( localStorage.getItem('items') );
+      if( !this.items ){
+        this.items = [];
+      }
+    }
+  }
+})
