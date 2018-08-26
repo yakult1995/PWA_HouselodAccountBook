9618255e836a218973fa13a226e4f17884f0625c
Yakult <destin.tax@outlook.jp>
2018-08-26 16:03:59 -0700

    version管理機能

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 const vm = new Vue({
   el: '#book',
   data: {
+    old_version: '',
     items: [],
     newName: '',
     newBalance: '',
@@ -12,8 +13,23 @@ const vm = new Vue({
     this.loadTodo();
     this.calTotal();
     this.newDate = this.myDate && this.myDate.toISOString().split('T')[0];
+
+    // version check
+    this.checkVersion();
   },
   methods: {
+    checkVersion: function(){
+        this.old_version = JSON.parse(localStorage.getItem('version'));
+        if(!this.old_version){
+            this.old_version = {'version' : '0'};
+        }
+        if($('#current_version').text() != this.old_version['version']){
+            alert('version up !');
+            localStorage.setItem('version', JSON.stringify({
+                'version' : $('#current_version').text()
+            }));
+        }
+    },
     calTotal: function(){
         this.totalBill = 0.0;
         for(var i = 0; i < this.items.length; i++){
@@ -53,9 +69,9 @@ const vm = new Vue({
     },
     loadTodo: function(){
         this.items = JSON.parse( localStorage.getItem('items') );
-            if( !this.items ){
-                this.items = [];
-            }
+        if(!this.items){
+            this.items = [];
         }
     }
+  }
 })
