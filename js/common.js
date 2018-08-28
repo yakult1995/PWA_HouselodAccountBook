b7ab7730fc2dced32e0ac47e546484f0f34f9153
Yakult <destin.tax@outlook.jp>
2018-08-27 17:43:53 -0700

    V2.1.2

---
+++ js/common.js
@@ -0,0 +1,103 @@
+const vm = new Vue({
+  el: '#book',
+  data: {
+    old_version: '',
+    items: [],
+    newName: '',
+    newBalance: '',
+    totalBill: 0.0,
+    newDate: '',
+    myDate: new Date(),
+    importedData: '',
+    disp_day: ''
+  },
+  mounted: function(){
+    this.loadTodo();
+    this.calTotal();
+    this.newDate = this.myDate && this.myDate.toISOString().split('T')[0];
+
+    // version check
+    this.checkVersion();
+  },
+  methods: {
+    setDsipDay:function(date){
+        if(this.disp_day == ''){
+            this.disp_day = date;
+        }else{
+            this.disp_day = '';
+        }
+    },
+    isDispDay: function(date){
+        if(this.disp_day == ''){
+            return true;
+        }else if(this.disp_day == date){
+            return true;
+        }else{
+            return false;
+        }
+    },
+    importData: function(importedData){
+        this.importedData = importedData.split('\n');
+        for(var i = 0; i < this.importedData.length; i++){
+            var temp_data = this.importedData[i].split(',');
+            this.addTodo(temp_data[0], temp_data[1], temp_data[2]);
+        }
+        this.importedData = '';
+    },
+    checkVersion: function(){
+        this.old_version = JSON.parse(localStorage.getItem('version'));
+        if(!this.old_version){
+            this.old_version = {'version' : '0'};
+        }
+        if($('#current_version').text() != this.old_version['version']){
+            alert('version up !');
+            localStorage.setItem('version', JSON.stringify({
+                'version' : $('#current_version').text()
+            }));
+        }
+    },
+    calTotal: function(){
+        this.totalBill = 0.0;
+        for(var i = 0; i < this.items.length; i++){
+            this.totalBill += parseFloat(this.items[i].balance);
+        }
+    },
+    addTodo: function(newName, newBalance, newDate){
+        this.items.push({
+            name: newName,
+            balance: newBalance,
+            date: newDate
+        });
+        this.newName = '';
+        this.newBalance = '';
+        this.newDate = this.myDate && this.myDate.toISOString().split('T')[0];
+        this.saveTodo();
+    },
+    deleteTodo: function(ele){
+        if(confirm(ele.name + 'を削除しますか？')){
+            this.items = this.items.filter(function(item){
+                return item.name != ele.name;
+            })
+            this.saveTodo();
+        }
+    },
+    saveTodo: function(){
+        // sort
+        this.items.sort(function(a, b){
+            if(a.date < b.date) return 1;
+            if(a.date > b.date) return -1;
+            return 0;
+        });
+
+        localStorage.setItem('items', JSON.stringify(this.items));
+
+        this.calTotal();
+    },
+    loadTodo: function(){
+        this.items = JSON.parse( localStorage.getItem('items') );
+        if(!this.items){
+            this.items = [];
+        }
+    }
+  }
+})
---
+++ sw.js
@@ -0,0 +1,63 @@
+var CACHE_STATIC_VERSION = 'v.2.1.2';
+var urlsToCache = [
+    'sw.js',
+    'manifest.json',
+    'index.html',
+    'css/style.css',
+    'js/common.js',
+    'css/bootstrap4.1.3.min.css',
+    'js/jquery-3.3.1.min.js',
+    'js/popper.1.14.3.min.js',
+    'js/vue.min.js',
+    'js/bootstrap4.1.3.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_STATIC_VERSION)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(key) {
+            if (key !== CACHE_STATIC_VERSION) {
+                console.log('DELETE CACHE : ' + key);
+                return caches.delete(key);
+            }
+        }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_STATIC_VERSION)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
