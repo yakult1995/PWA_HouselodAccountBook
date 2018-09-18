c5a1ae2972acb3155de1eaef3b005a6a83f31462
Yakult <destin.tax@outlook.jp>
2018-09-18 11:52:45 -0700

    レイアウト変更

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_STATIC_VERSION = 'v.2.2.7';
+var CACHE_STATIC_VERSION = 'v.2.2.8';
 var urlsToCache = [
     'https://fonts.googleapis.com/css?family=Quicksand',
     'https://fonts.googleapis.com/earlyaccess/notosansjapanese.css',
